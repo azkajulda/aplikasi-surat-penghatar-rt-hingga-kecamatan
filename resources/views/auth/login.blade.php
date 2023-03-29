544494eee449afc07376e0b427761e9c23d498be
azkajulda <azkajulda@gmail.com>
2023-03-29 10:21:55 +0700

    Feature Login and authentication

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+
+	<form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
+		@csrf
+		<span class="login100-form-title">
+			Log in Account 
+		</span>
+
+		<div class="wrap-input100 validate-input" data-validate = "@error('username') {{$message}} @enderror">
+			<input class="input100" placeholder="NIK / Email" id="username" type="text" name="username" value="{{ old('username') }}" required autofocus>
+			<span class="focus-input100"></span>
+			<span class="symbol-input100">
+				<i class="fa fa-user-circle-o" aria-hidden="true"></i>
+			</span>
+		</div>
+
+		<div class="wrap-input100 validate-input" data-validate = "@error('password') {{$message}} @enderror">
+			<input class="input100" placeholder="Password" type="password" name="password" required autocomplete="current-password">
+			<span class="focus-input100"></span>
+			<span class="symbol-input100">
+				<i class="fa fa-lock" aria-hidden="true"></i>
+			</span>
+		</div>
+
+		@error('username')
+			<div class="txtDanger text-center"> 
+					<span role="alert">
+							<strong>{{ $message }}</strong>
+					</span>
+			</div>
+		@enderror
+		
+		<div class="container-login100-form-btn">
+			<button class="login100-form-btn" type="submit">
+				Login
+			</button>
+		</div>
+
+		<div class="text-center p-t-50">
+			<span class="txt1">
+				Forgot
+			</span>
+			<a class="txt2" href="{{ route('password.request') }}">
+				Password?
+			</a>
+		</div>
+	</form>
+
+@endsection
