15271d3a371b88230d1abe2b4f745d0c07ba2d04
fuadzulfikar29 <mfuadzulfikar@gmail.com>
2023-06-09 20:30:15 +0700

    tambah akun lurah

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,10 +19,8 @@ class UserSeeder extends Seeder
     {
         $now = Carbon::now()->toDateTimeString();
         User::insert([
-            ['no_kk' => '32731310121999', 'email' => 'azk@gmail.com', 'password' => bcrypt('warga123'), 'created_at' => $now, 'updated_at' => $now, 'role' => 'warga'],
-            ['no_kk' => '123', 'email' => '123@gmail.com', 'password' => bcrypt('rt123'), 'created_at' => $now, 'updated_at' => $now, 'role' => 'rt'],
-            ['no_kk' => '321', 'email' => '321@gmail.com', 'password' => bcrypt('rw123'), 'created_at' => $now, 'updated_at' => $now, 'role' => 'rw'],
-            ['no_kk' => '456', 'email' => '456@gmail.com', 'password' => bcrypt('lurah123'), 'created_at' => $now, 'updated_at' => $now, 'role' => 'lurah']
+            ['no_kk' => '456', 'email' => '456@gmail.com', 'password' => bcrypt('lurah123'), 'created_at' => $now, 'updated_at' => $now, 'role' => 'lurah'],
+            ['no_kk' => 'kelurahan', 'email' => 'kelurahan@gmail.com', 'password' => bcrypt('lurah123'), 'created_at' => $now, 'updated_at' => $now, 'role' => 'lurah']
         ]);
     }
 }
